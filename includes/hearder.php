c8042549a14fc0ac6a51035407f1e04a2dc15e0e
jado <tony76.200@hotmail.fr>
2018-04-04 16:33:30 +0200

    ajouté le style css et ajout d'une page function

--- includes/hearder.php
+++ includes/hearder.php
@@ -1,6 +1,6 @@
 <header>
-    <nav id="menu">
-        <ul>
+    <nav>
+        <ul id="menu">
             <li><a href="index.php?page=home">Acceuil</a></li>
             <li><a href="index.php?page=registration">Inscription</a></li>
             <li><a href="index.php?page=contact">Contact</a></li>
--- index.php
+++ index.php
@@ -1,11 +1,37 @@
+<?php
+    include "./functions/callPage.php";
+?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
+	<meta charset="utf-8">
+	<title>Page</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    
 </head>
 
-<?php
-include "./functions/callPage.php";
-include "./includes/hearder.php";
 
-callPage();
+<body>
+    <div id="container">
+        <?php include "./includes/hearder.php"; ?>
+    <main>
+        <?php callPage(); ?>
+    </main>
+        <?php include "./includes/footer.php"; ?>
+</div>
 
-include "./includes/footer.php";
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
