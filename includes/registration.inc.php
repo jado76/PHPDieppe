6adc74d18bef61ad0460d624a4191915e823cb14
jado <tony76.200@hotmail.fr>
2018-04-05 11:00:15 +0200

    affichage des erreurs

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,8 +1,29 @@
 <h1>Iscription</h1>
 <?php
 if (isset($_POST['formRegistration'])) {
-    echo "Je viens du formulaire";
-    include "formRegistration.php";
+
+    $nom = $_POST['nom'] ?? "";
+    $prenom = $_POST['prenom'] ?? "";
+    $mail = $_POST['mail'] ?? "";
+    $mdp = $_POST['mdp'] ?? "";
+
+
+    $erreurs = array();
+
+    if ($nom == "") array_push($erreurs, "Veuillez saisir votre nom");
+    if ($prenom == "") array_push($erreurs, "Veuillez saisir votre prénom");
+    if ($mail == "") array_push($erreurs, "Veuillez saisir votre mail");
+    if ($mdp == "") array_push($erreurs, "Veuillez saisir votre mot de passe");
+
+    if(count($erreurs) > 0) {
+        echo "Erreurs";
+    }
+
+    else {
+        echo "Pas d'erreurs";
+    }
+
+
 }
 
 else {
