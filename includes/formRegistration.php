676b3c73fea7beb00d719ee73a101bcf0b3419bc
jado <tony76.200@hotmail.fr>
2018-04-05 09:28:23 +0200

    ecriture formulaire inscription

---
+++ includes/formRegistration.php
@@ -0,0 +1,24 @@
+<form method="post" action="#">
+    <div>
+        <label for="nom">Nom&nbsp; :</label>
+        <input type="texte" name="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prénom&nbsp; :</label>
+        <input type="texte" name="prenom" />
+    </div>
+    <div>
+        <label for="mail">Mail&nbsp; :</label>
+        <input type="texte" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe&nbsp; :</label>
+        <input type="password" name="mpd" />
+    </div>
+    <div>
+        <input type="reset" value="effacer" />
+        <input type="submit" value="envoyer" />
+    </div>
+    <input type="hidden" name="formRegistration" />
+    
+</form>
