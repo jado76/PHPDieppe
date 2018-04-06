a885039af54d078aaa4ef6a1c19eb0592e524a3e
jado <tony76.200@hotmail.fr>
2018-04-06 10:17:55 +0200

    correction orthographe

--- includes/hearder.php
+++ includes/hearder.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul id="menu">
-            <li><a href="index.php?page=home">Acceuil</a></li>
+            <li><a href="index.php?page=home">Accueil</a></li>
             <li><a href="index.php?page=registration">Inscription</a></li>
             <li><a href="index.php?page=contact">Contact</a></li>
             <li><a href="index.php?page=news">News</a></li>
